e442eb67ae0ccf47295818221a3d11aff0890576
romain <romain.de-matos-ribeiro@epitech.eu>
2024-03-05 14:26:20 +0100

    update

--- trello_app/lib/src/screens/register_screen.dart
+++ trello_app/lib/src/screens/register_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class BoardWidget extends StatelessWidget{
+class RegisterScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
